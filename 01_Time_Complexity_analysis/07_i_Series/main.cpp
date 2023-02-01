4dc7225c687d5fa6f149c743c5e8643fae415bcd
GoesToTwentyOne <77912479+GoesToTwentyOne@users.noreply.github.com>
2023-02-02 01:26:38 +0600

    series

---
+++ 01_Time_Complexity_analysis/07_i_Series/main.cpp
@@ -0,0 +1,17 @@
+
+
+  for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+        cout<<i<<j<<\n
+      }
+    }
+    /*
+    iteration
+    i---->j
+    0     n
+    1     n-1
+    ..
+    n-1   1
+    n=n(n+1)/2
+    */
+    //Time complexity=O(n^2)

b70be30e475043d6f77cbc963f1fa198596cf728
GoesToTwentyOne <77912479+GoesToTwentyOne@users.noreply.github.com>
2023-02-02 01:16:22 +0600

    done

---
+++ 01_Time_Complexity_analysis/05_O(n+q)/main.cpp
@@ -0,0 +1,10 @@
+for(int i=0;i<n;i++){
+    cout << i << '\n';
+  }
+  //Time complexity=O(n);
+
+  for(int i=0;i<q;i++){
+      cout << i << '\n';
+    }
+    //Time complexity=O(q)
+    //Total time complexity=O(n+q)

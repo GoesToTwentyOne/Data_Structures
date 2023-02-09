0f5536d52df5aa5aa3411c8549fb18ce26fc908c
GoesToTwentyOne <77912479+GoesToTwentyOne@users.noreply.github.com>
2023-02-10 03:59:40 +0600

    STL queue

---
+++ 06_Queue_data_structure/05_build_in_STL_queue/main.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  queue<int> q;
+  q.push(12);
+  q.push(14);
+  q.push(16);
+  cout<<q.size()<<'\n';
+  cout<<q.empty()<<'\n';
+  q.pop();
+  cout<<q.front()<<'\n';
+
+
+
+}

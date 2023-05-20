ccd0580e57198b8be770ca62bea2368688fc51cb
GoesToTwentyOne <hnihadgo75@gmail.com>
2023-05-21 01:31:24 +0600

    bubble sort all done

--- 02_Sorting_Algorithms/01_Bubble_sort/main.cpp
+++ 02_Sorting_Algorithms/01_Bubble_sort/main.cpp
@@ -9,12 +9,17 @@ int main() {
   }
   for(int pass=0;pass<n;pass++){
     int last=n-1-pass;
+    bool sorted = true;//most optimization
     for(int j=0;j<=last-1;j++){
       if(arr[j] > arr[j+1]) {
         swap(arr[j],arr[j+1]);
+        sorted=false;
       }
     }
-    cout<<"after pass: "<<pass<<", ";
+    if(sorted){ //optimize passes
+      break;
+    }
+    cout<<"after pass: "<<pass<<", \t";
     for(int i=0;i<n;i++){
       cout<<" "<<arr[i];
     }

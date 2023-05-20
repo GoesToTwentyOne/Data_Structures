01cd94c5a49afc085109ae8bef37704eda6b8403
GoesToTwentyOne <hnihadgo75@gmail.com>
2023-05-21 04:56:12 +0600

    merge sort complete

--- 02_Sorting_Algorithms/03_Merge_Sort/main.cpp
+++ 02_Sorting_Algorithms/03_Merge_Sort/main.cpp
@@ -1,19 +1,32 @@
 #include<bits/stdc++.h>
 using namespace std;
-
 vector<int> merge_Sort(vector<int> a){
   if(a.size()<=1){
     return a;
   }
   int mid = a.size()/2;
+  //printing for simulation
+  cout<<"Middle Point:"<<mid<<'\n';
   vector<int> b;
   vector<int> c;
   for(int i=0;i<mid;i++){
     b.push_back(a[i]);
   }
+  //printing for simulation
+  cout<<"Smallest Part1: ";
+  for(int i=0;i<b.size();i++){
+    cout<<b[i]<<" ";
+  }
+  cout<<'\n';
   for(int i=mid;i<a.size();i++){
     c.push_back(a[i]);
   }
+  //printing for simulation
+  cout<<"Smallest Part2: ";
+  for(int i=0;i<c.size();i++){
+    cout<<c[i]<<" ";
+  }
+  cout<<'\n';
   vector<int> sorted_b = merge_Sort(b);
   vector<int> sorted_c = merge_Sort(c);
 
@@ -44,6 +57,7 @@ vector<int> merge_Sort(vector<int> a){
 int main(){
   vector<int> a = {5,78,12,2,3,6,8,88,16};
   vector<int> result = merge_Sort(a);
+  cout<<"after devide and coqure final sorting:"<<'\n';
   for(int i=0;i<result.size();i++){
     cout << result[i] << " ";
   }

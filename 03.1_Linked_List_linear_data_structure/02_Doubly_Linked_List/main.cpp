badeee7baaaaed3821b8b9850988eb48de6c68eb
GoesToTwentyOne <hnihadgo75@gmail.com>
2023-05-22 13:09:38 +0600

    doubly linked list all done

--- 03.1_Linked_List_linear_data_structure/02_Doubly_Linked_List/main.cpp
+++ 03.1_Linked_List_linear_data_structure/02_Doubly_Linked_List/main.cpp
@@ -1,111 +1,150 @@
-**Doubly Linked List**
+#include<bits/stdc++.h>
+using namespace std;
+class NODE{
+public:
+  int data;
+  NODE *next_pointer;
+  NODE *previous_pointer;
+};
+class DoublyLinkedList{
+public:
+  NODE *head;
+  int size;
+  DoublyLinkedList(){
+    head=NULL;
+    size=0;
+  }
+  NODE* createNode(int value){
+    NODE *newNode=new NODE;
+    newNode->data=value;
+    newNode->next_pointer=NULL;
+    newNode->previous_pointer=NULL;
+    return newNode;
+  }
+  void insert_At_Head(int value){
+    size++;
+    NODE *newNode= createNode(value);
+    if(head == NULL){
+      head=newNode;
+      return;
+    }
+    newNode->next_pointer=head;
+    head->previous_pointer=newNode;
+    head=newNode;
+  }
+  void insert_At_Any_pos(int pos,int value){
+    size++;
+    NODE *newNode = createNode(value);
+    if (pos == 0) {
+      insert_At_Head(value);
+      return;
+    }
+    NODE *a=head;
+    int current_index=0;
+    while(current_index!=pos-1){
+      a=a->next_pointer;
+      current_index++;
+    }
+    NODE *b=a->next_pointer;
+    newNode->next_pointer=a->next_pointer;
+    newNode->previous_pointer=a;
+    b->previous_pointer=newNode;
+    a->next_pointer=newNode;
+  }
+  void insert_At_Last(int value){
+    size++;
+    NODE *newNode = createNode(value);
+    if (head == NULL) {
+      insert_At_Head(value);
+      return;
+    }
+    NODE *a = head;
+    while (a->next_pointer != NULL) {
+      a = a->next_pointer;
+    }
+    a->next_pointer = newNode;
+    newNode->previous_pointer = a;
+  }
+  void delete_At_Head(){
 
-- A doubly linked list is a data structure that consists of a sequence of nodes, where each node contains a reference to the previous node and the next node in the sequence. This allows for efficient traversal in both directions (forward and backward) within the list.
-This repository provides a simulation and implementation of a doubly linked list, along with an example and detailed explanations of its working, complexity analysis, advantages, disadvantages, algorithms, pseudocode, memory representation, memory allocation, garbage collection, and limitations.
+    if(head == NULL){
+      cout<<"underflow"<<'\n';
+    }
+    NODE *a=head;
+    NODE *b=a->next_pointer;
+    head=b;
+    b->previous_pointer=NULL;
+    delete a;
+    size--;
+  }
+  void delete_At_Any_Pos(int pos){
+    if(pos==0){
+      delete_At_Head();
+      return;
+    }
+    if(pos==size){
+      delete_At_Last();
+      return;
+    }
+    NODE *a= head;
+    int current_index=0;
+    while(current_index != pos){
+      a=a->next_pointer;
+      current_index++;
+    }
+    NODE *b=a->previous_pointer;
+    NODE *c=a->next_pointer;
+    if(b!= NULL){
+      b->next_pointer=c;
+    }
+    if(c!= NULL){
+      c->previous_pointer=b;
+    }
+    delete a;
+    size--;
+  }
+  void delete_At_Last(){
+    NODE *a=head;
+    while(a->next_pointer->next_pointer!=NULL){
+      a=a->next_pointer;
+    }
+    NODE *b=a->next_pointer;
+    a->next_pointer=NULL;
+    delete b;
+    size --;
+  }
+  void traverse(){
+    NODE *a=head;
+    while(a!=NULL){
+      cout<<a->data<<" ";
+      a=a->next_pointer;
+    }
+    cout<<'\n';
 
-**Example**
-- Consider a scenario where you want to implement a playlist in a music player application. Each song in the playlist can be represented as a node in the doubly linked list. The doubly linked list allows you to easily navigate through the playlist, both forwards and backwards, and perform operations such as adding a song, removing a song, and shuffling the playlist.
-
-**How It Works**
-- A doubly linked list is composed of nodes, where each node has three components: data, previous, and next. The data component holds the actual value or payload of the node, while the previous and next components store references to the previous and next nodes in the list, respectively.
-- To traverse the doubly linked list, you typically start from the head (the first node) and follow the next references until you reach the desired node. For backward traversal, you start from the tail (the last node) and follow the previous references.
-- The doubly linked list allows for efficient insertion and deletion of nodes at any position in the list, as you only need to update the references of the adjacent nodes.
-
-**Complexity Analysis**
-- The complexity analysis for various operations in a doubly linked list is as follows:
-
-- Traversal: O(n)
-- Insertion at the beginning: O(1)
-- Insertion at the end: O(1)
-- Insertion at a given position: O(n)
-- Deletion at the beginning: O(1)
-- Deletion at the end: O(1)
-- Deletion at a given position: O(n)
-- Search: O(n)
-
-**Advantages**
-- Efficient traversal in both directions (forward and backward).
-- Insertion and deletion can be performed at any position with a time complexity of O(n).
-- It allows easy implementation of advanced data structures like queues and stacks.
-
-**Disadvantages**
-- Higher memory overhead compared to a singly linked list due to the additional reference to the previous node.
-- Extra care must be taken to maintain the integrity of the list, as updating references can be error-prone.
-- Requires more memory to store the additional reference for each node.
-
-**Algorithm**
-- The following algorithm outlines the basic operations of a doubly linked list:
-- Create a node with the given data.
-- If the list is empty, set the new node as the head and tail.
-- If the list is not empty, set the next reference of the current tail to the new node, and set the previous reference of the new node to the current tail.
-Update the tail to be the new node.
-- Repeat steps 1-4 as needed for other operations like insertion at the beginning or at a given position.
-- To delete a node, update the next and previous references of the adjacent nodes to bypass the node to be deleted.
-
-
-**Pseudocode**
-- The following pseudocode provides an example implementation of a doubly linked list:
-
-- class Node:
-    - data
-    - previous
-    - next
-
-- class DoublyLinkedList:
-    - head
-    - tail
-
-    - function insertAtBeginning(data):
-        - newNode = new Node(data)
-        - if head is null:
-            - head = newNode
-            - tail = newNode
-        - else:
-            - newNode.next = head
-            - head.previous = newNode
-            - head = newNode
-
-    - function insertAtEnd(data):
-        - newNode = new Node(data)
-        - if tail is null:
-            - head = newNode
-            - tail = newNode
-        - else:
-            - newNode.previous = tail
-            - tail.next = newNode
-            - tail = newNode
-
-    - function insertAtPosition(data, position):
-        // Implementation left as an exercise
-
-    - function deleteAtBeginning():
-        // Implementation left as an exercise
-
-    - function deleteAtEnd():
-        // Implementation left as an exercise
-
-    - function deleteAtPosition(position):
-        // Implementation left as an exercise
+  }
+  int getSize(){
+    return size;
+  }
+};
+int main(){
+  DoublyLinkedList dl;
+  dl.insert_At_Head(10);
+  dl.insert_At_Head(20);
+  dl.insert_At_Last(30);
+  dl.insert_At_Last(500);
+  dl.insert_At_Head(10);
+  dl.insert_At_Head(20);
+  dl.insert_At_Last(30);
+  dl.insert_At_Last(500);
+  dl.traverse();
+  dl.insert_At_Any_pos(2,800);
+  dl.traverse();
+  dl.delete_At_Head();
+  dl.traverse();
+  dl.delete_At_Last();
+  dl.traverse();
+  dl.delete_At_Any_Pos(2);
+  dl.traverse();
 
 
-**Memory Representation with Calculation**
-- In a doubly linked list, each node contains the following components:
-
-- data (payload): The value or data stored in the node.
-- previous: A reference to the previous node in the list.
-- next: A reference to the next node in the list.
-- The memory required to represent a doubly linked list depends on the number of nodes in the list. For each node, memory is allocated to store the data and the references to the previous and next nodes. Additionally, memory is allocated for the head and tail references of the list.
-
-- Suppose each node requires M memory units to store the data and references, and there are N nodes in the list. The total memory required for the doubly linked list is approximately (N + 2) * M units.
-
-**Memory Allocation**
-- Memory allocation for a doubly linked list can be performed dynamically using programming languages that support dynamic memory allocation, such as C, C++, or Java. Each time a new node is inserted, memory is allocated for the node, and the appropriate references are updated.
-
-**Garbage Collection**
-- Garbage collection is the process of automatically reclaiming memory that is no longer in use by the program. In languages with automatic garbage collection, such as Java or Python, the memory occupied by nodes that are no longer reachable (e.g., due to removal from the list) is automatically reclaimed by the garbage collector.
-
-**Limitations of Doubly Linked List**
-- Increased memory overhead due to the additional reference to the previous node.
-- Slower insertion and deletion compared to arrays or singly linked lists, especially when dealing with large lists or performing operations at arbitrary positions.
-- Complexity in maintaining the integrity of the list, as updating references can be error-prone.
-- Extra memory requirements compared to a singly linked list.
+}

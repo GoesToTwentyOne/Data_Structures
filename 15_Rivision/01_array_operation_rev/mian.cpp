2ebeedb1d4beb1cb75b2771f087c6dadef6f03f5
GoesToTWentyOne <hnihadgo75@gmail.com>
2024-07-06 07:43:32 +0600

    done

--- 15_Rivision/01_array_operation_rev/mian.cpp
+++ 15_Rivision/01_array_operation_rev/mian.cpp
@@ -19,6 +19,17 @@ private:
         delete[] arr;
         arr = temp;
     }
+    void arrayDecrease()
+    {
+        capacity = capacity / 2;
+        int *temp = new int[capacity];
+        for (int i = 0; i < size; i++)
+        {
+            temp[i] = arr[i];
+        }
+        delete[] arr;
+        arr = temp;
+    }
 
 public:
     ArrayAll()
@@ -41,7 +52,43 @@ public:
         arr[0] = value;
         size++;
     }
-
+    void arrayPos(int pos, int value)
+    {
+        if (size == capacity)
+        {
+            arrayIncrease();
+        }
+        for (int i = size - 1; i >= pos; i--)
+        {
+            arr[i + 1] = arr[i];
+        }
+        arr[pos] = value;
+        size++;
+    }
+    void arrayDelFir()
+    {
+        if (size == capacity / 2)
+        {
+            arrayDecrease();
+        }
+        for (int i = 0; i < size; i++)
+        {
+            arr[i] = arr[i + 1];
+        }
+        size--;
+    }
+    void arrayDelPos(int pos)
+    {
+        if (size == capacity / 2)
+        {
+            arrayDecrease();
+        }
+        for (int i = pos + 1; i >= pos; i--)
+        {
+            arr[i - 1] = arr[1];
+        }
+        size--;
+    }
     void arrayPrint()
     {
         for (int i = 0; i < size; i++)
@@ -58,6 +105,12 @@ int main()
     a.arrayFirstInsert(10);
     a.arrayFirstInsert(20);
     a.arrayFirstInsert(30);
+    a.arrayPrint();
+    a.arrayPos(2, 77777);
+    a.arrayPrint();
+    a.arrayDelFir();
+    a.arrayPrint();
+    a.arrayDelPos(2);
     a.arrayPrint();
     return 0;
 }

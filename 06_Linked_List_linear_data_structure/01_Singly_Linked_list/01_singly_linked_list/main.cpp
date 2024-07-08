74612a410337c8ebda5569cb86581c5c3296da86
GoesToTWentyOne <hnihadgo75@gmail.com>
2024-07-08 06:38:43 +0600

    done single linked list

--- 06_Linked_List_linear_data_structure/01_Singly_Linked_list/01_singly_linked_list/main.cpp
+++ 06_Linked_List_linear_data_structure/01_Singly_Linked_list/01_singly_linked_list/main.cpp
@@ -1,172 +1,241 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-class Node{
+
+class Node
+{
 public:
   int data;
   Node *next_pointer;
 };
-class Linked_List{
+
+class Linked_List
+{
 public:
   Node *head;
   int size;
-  Linked_List(){
-    head=NULL;
-    size=0;
+  Linked_List()
+  {
+    head = NULL;
+    size = 0;
   }
-  //create a new node : data= value, next_pointer=NULL
-  Node* createNode(int value){
-    Node *newnode=new Node;
-    newnode->data=value;
-    newnode->next_pointer= NULL;
+  // create a new node : data= value, next_pointer=NULL
+  Node *createNode(int value)
+  {
+    Node *newnode = new Node;
+    newnode->data = value;
+    newnode->next_pointer = NULL;
     return newnode;
   }
-  //Time complexity=O(1)
-  void insert_At_Head(int value){
+  // Time complexity=O(1)
+  void insert_At_Head(int value)
+  {
     size++;
-    Node *a= createNode(value);
-    if(head==NULL){
-      head=a;
+    Node *a = createNode(value);
+    if (head == NULL)
+    {
+      head = a;
       return;
     }
-    a->next_pointer=head;
-    head=a;
+    a->next_pointer = head;
+    head = a;
   }
-  //Time complexity= O(n)
-  void insert_At_Last(int value){
+  // Time complexity= O(n)
+  void insert_At_Last(int value)
+  {
     size++;
-    Node* a = head;
-    while (a->next_pointer != NULL) {
-        a = a->next_pointer;
+    Node *a = head;
+    if (a == NULL)
+    {
+      head = createNode(value);
+      return;
+    }
+    while (a->next_pointer != NULL)
+    {
+      a = a->next_pointer;
     }
     a->next_pointer = createNode(value);
   }
-  //Time complexity= O(n)
-  void insert_At_Any_pos(int pos,int value){
-    if(pos>size){
-      cout<<"Overflow\n";
+  // Time complexity= O(n)
+  void insert_At_Any_pos(int pos, int value)
+  {
+    if (pos > size || pos < 0)
+    {
+      cout << "Overflow\n";
+      return;
     }
-    if(pos==0){
+    if (pos == 0)
+    {
       insert_At_Head(value);
+      return;
     }
-    if(pos==size){
+    if (pos == size)
+    {
       insert_At_Last(value);
+      return;
     }
     size++;
-    int current_index=0;
-    Node* a=head;
-    while(current_index != pos-1){
-      a=a->next_pointer;
+    int current_index = 0;
+    Node *a = head;
+    while (current_index != pos - 1)
+    {
+      a = a->next_pointer;
       current_index++;
     }
-    Node* newnode= createNode(value);
-    newnode->next_pointer=a->next_pointer;
-    a->next_pointer=newnode;
+    Node *newnode = createNode(value);
+    newnode->next_pointer = a->next_pointer;
+    a->next_pointer = newnode;
   }
-  void delete_At_Head(){
-    if(head==NULL){
-      cout<<"underflow"<<'\n';
+  void delete_At_Head()
+  {
+    if (head == NULL)
+    {
+      cout << "underflow" << '\n';
       return;
     }
     size--;
-    Node* a=head;
-    head=a->next_pointer;
+    Node *a = head;
+    head = a->next_pointer;
     delete a;
   }
-  void delete_At_Any_Pos(int pos){
-    if(pos==0){
+  void delete_At_Any_Pos(int pos)
+  {
+    if (pos < 0 || pos >= size)
+    {
+      cout << "Invalid position\n";
+      return;
+    }
+    if (pos == 0)
+    {
       delete_At_Head();
+      return;
     }
     size--;
-    int current_index=0;
-    Node* a=head;
-    while(current_index != pos-1){
-      a=a->next_pointer;
-    }
-    Node* b=a->next_pointer;
-    a->next_pointer=b->next_pointer;
-    delete b;
-  }
-  void delete_At_Last(){
-
-    Node* a=head;
-    while(a->next_pointer->next_pointer!=NULL){
-      a=a->next_pointer;
+    int current_index = 0;
+    Node *a = head;
+    while (current_index != pos - 1)
+    {
+      a = a->next_pointer;
+      current_index++;
     }
     Node *b = a->next_pointer;
-    a->next_pointer=NULL;
+    a->next_pointer = b->next_pointer;
     delete b;
   }
-  //Time complexity= O(n)
-  void traverse(){
-    Node* a=head;
-    //while(a->next_pointer!=NULL){
-    while(a!=NULL){
-      cout<<a->data<<" ";
-      a=a->next_pointer;
+  void delete_At_Last()
+  {
+    if (head == NULL)
+    {
+      cout << "underflow" << '\n';
+      return;
     }
-    cout<<'\n';
+    if (head->next_pointer == NULL)
+    {
+      delete head;
+      head = NULL;
+      size--;
+      return;
+    }
+    Node *a = head;
+    while (a->next_pointer->next_pointer != NULL)
+    {
+      a = a->next_pointer;
+    }
+    Node *b = a->next_pointer;
+    a->next_pointer = NULL;
+    delete b;
+    size--;
   }
-  int distinctSearch(int value){
-    Node* a=head;
-    int index=0;
-    //while(a->next_pointer!=NULL){
-    while(a!=NULL){
-      if(a->data==value){
+  // Time complexity= O(n)
+  void traverse()
+  {
+    Node *a = head;
+    while (a != NULL)
+    {
+      cout << a->data << " ";
+      a = a->next_pointer;
+    }
+    cout << '\n';
+  }
+  int distinctSearch(int value)
+  {
+    Node *a = head;
+    int index = 0;
+    while (a != NULL)
+    {
+      if (a->data == value)
+      {
         return index;
       }
-      a=a->next_pointer;
+      a = a->next_pointer;
       index++;
     }
     return -1;
-
   }
-  void allPossibleSearch(int value){
-    Node* a=head;
-    int index=0;
-    while(a!=NULL){
-      if(a->data==value){
-        cout<<value<<" is found at :"<<index<<"\t";
+  void allPossibleSearch(int value)
+  {
+    Node *a = head;
+    int index = 0;
+    while (a != NULL)
+    {
+      if (a->data == value)
+      {
+        cout << value << " is found at :" << index << "\t";
       }
-      a=a->next_pointer;
+      a = a->next_pointer;
       index++;
     }
-    cout<<'\n';
+    cout << '\n';
   }
 
-
-
-
-  //insert after a value
-  void after_A_value(int value,int data){
-    Node *a=head;
-    int current_index=0;
-    while (a!=NULL) {
-      if(a->data==value){
-          break;
-      }
-      a=a->next_pointer;
-    }
-    if (a==NULL){
-      cout<<"value doesn't exist"<<'\n';
+  void reverse_list(Node *a)
+  {
+    if (a == NULL)
+    {
       return;
     }
-    Node *newnode= createNode(data);
-    newnode->next_pointer=a->next_pointer;
-    a->next_pointer=newnode;
+    if (a->next_pointer != NULL)
+    {
+      reverse_list(a->next_pointer);
+    }
+    cout << a->data << " ";
+  }
+
+  // insert after a value
+  void after_A_value(int value, int data)
+  {
+    Node *a = head;
+    while (a != NULL)
+    {
+      if (a->data == value)
+      {
+        break;
+      }
+      a = a->next_pointer;
+    }
+    if (a == NULL)
+    {
+      cout << "value doesn't exist" << '\n';
+      return;
+    }
+    Node *newnode = createNode(data);
+    newnode->next_pointer = a->next_pointer;
+    a->next_pointer = newnode;
   }
 };
-int main(){
+
+int main()
+{
   Linked_List li;
   li.insert_At_Head(10);
   li.insert_At_Head(20);
   li.insert_At_Head(30);
   li.insert_At_Head(20);
   li.traverse();
-  cout<<"20 is found at : "<<li.distinctSearch(20)<<'\n';
+  cout << "20 is found at : " << li.distinctSearch(20) << '\n';
   li.allPossibleSearch(20);
   li.insert_At_Last(4848);
   li.traverse();
-  li.insert_At_Any_pos(2,45456);
+  li.insert_At_Any_pos(2, 45456);
   li.traverse();
   li.delete_At_Head();
   li.traverse();
@@ -174,6 +243,9 @@ int main(){
   li.traverse();
   li.delete_At_Last();
   li.traverse();
-  li.after_A_value(30,100);
+  li.after_A_value(30, 100);
+  cout << "Reversed list: ";
+  li.reverse_list(li.head);
+  cout << endl;
   li.traverse();
 }
--- 15_Rivision/03_Linked_list/main.cpp
+++ 15_Rivision/03_Linked_list/main.cpp
@@ -1,21 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 class Node
 {
 public:
     int data;
     Node *next_pointer;
 };
+
 class Linked_List
 {
 public:
     Node *head;
     int size;
+
     Linked_List()
     {
         head = NULL;
         size = 0;
     }
+
     Node *createNode(int value)
     {
         Node *newNode = new Node;
@@ -23,18 +27,15 @@ public:
         newNode->next_pointer = NULL;
         return newNode;
     }
+
     void insertAtBeginning(int value)
     {
         size++;
         Node *a = createNode(value);
-        if (head == NULL)
-        {
-            head = a;
-            return;
-        }
         a->next_pointer = head;
         head = a;
     }
+
     void insertAtEnd(int value)
     {
         size++;
@@ -51,10 +52,22 @@ public:
         }
         temp->next_pointer = a;
     }
+
     void insertAtPosition(int value, int position)
     {
+        if (position < 0 || position > size)
+        {
+            cout << "Position out of bounds" << endl;
+            return;
+        }
         size++;
         Node *a = createNode(value);
+        if (position == 0)
+        {
+            a->next_pointer = head;
+            head = a;
+            return;
+        }
         int current_index = 0;
         Node *temp = head;
         while (current_index != position - 1)
@@ -65,16 +78,35 @@ public:
         a->next_pointer = temp->next_pointer;
         temp->next_pointer = a;
     }
+
     void deleteAtBeginning()
     {
+        if (head == NULL)
+        {
+            cout << "List is empty" << endl;
+            return;
+        }
         Node *current = head;
         head = current->next_pointer;
         delete current;
+        size--;
     }
+
     void deleteAtEnd()
     {
+        if (head == NULL)
+        {
+            cout << "List is empty" << endl;
+            return;
+        }
+        if (head->next_pointer == NULL)
+        {
+            delete head;
+            head = NULL;
+            size--;
+            return;
+        }
         Node *temp = head;
-
         while (temp->next_pointer->next_pointer != NULL)
         {
             temp = temp->next_pointer;
@@ -82,9 +114,29 @@ public:
         Node *b = temp->next_pointer;
         temp->next_pointer = NULL;
         delete b;
+        size--;
     }
+
     void deleteAtPosition(int position)
     {
+        if (position < 0 || position >= size)
+        {
+            cout << "Position out of bounds" << endl;
+            return;
+        }
+        if (head == NULL)
+        {
+            cout << "List is empty" << endl;
+            return;
+        }
+        size--;
+        if (position == 0)
+        {
+            Node *temp = head;
+            head = head->next_pointer;
+            delete temp;
+            return;
+        }
         Node *temp = head;
         int current_index = 0;
         while (current_index != position - 1)
@@ -96,6 +148,7 @@ public:
         temp->next_pointer = b->next_pointer;
         delete b;
     }
+
     void traverse()
     {
         Node *temp = head;
@@ -104,7 +157,9 @@ public:
             cout << temp->data << " ";
             temp = temp->next_pointer;
         }
+        cout << endl; // Print newline at the end of the list
     }
+
     int search_node(int value)
     {
         Node *temp = head;
@@ -120,7 +175,21 @@ public:
         }
         return -1;
     }
+
+    void reverse_list(Node *a)
+    {
+        if (a == NULL)
+        {
+            return;
+        }
+        if (a->next_pointer != NULL)
+        {
+            reverse_list(a->next_pointer);
+        }
+        cout << a->data << " ";
+    }
 };
+
 int main()
 {
     Linked_List list;
@@ -134,5 +203,9 @@ int main()
     list.deleteAtEnd();
     list.deleteAtPosition(2);
     cout << list.search_node(50) << endl;
+
+    cout << "Reversed list: ";
+    list.reverse_list(list.head);
+    cout << endl; // Print newline at the end of the reversed list
     list.traverse();
 }
